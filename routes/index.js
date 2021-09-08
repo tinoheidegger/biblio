1d3bb4fd333a8b7c483005c73869d105037b0792
tinoheidegger <tinoheidegger83@gmail.com>
2021-09-08 00:50:02 +0000

    Initial Author Routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
-const { Router } = require("express");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("index");
 });
-
-router.post("/", (req, res) => {});
 
 module.exports = router;
